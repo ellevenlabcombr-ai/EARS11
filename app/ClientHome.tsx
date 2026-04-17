07a9aeda644dde5077a8aaf6cad7157f8f844c3d
ellevenlabcombr-ai <ellevenlab.com.br@gmail.com>
2026-04-17 16:28:40 -0300

    test login screen

--- app/ClientHome.tsx
+++ app/ClientHome.tsx
@@ -1,12 +1,7 @@
 "use client";
 
+import { LoginScreen } from '@/components/LoginScreen';
+
 export default function ClientHome() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>App carregou ✅</h1>
-      <button onClick={() => alert("clicou")}>
-        Teste botão
-      </button>
-    </div>
-  );
+  return <LoginScreen onLogin={() => {}} />;
 }
